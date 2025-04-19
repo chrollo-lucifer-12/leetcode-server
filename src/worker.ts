f7201e5f8a063a102650e62d11cab4d9a31ee293
sahil-singh1107 <sahilpython@gmail.com>
2025-04-19 15:40:23 +0530

    completed

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,37 @@
 import {Worker} from "bullmq"
-
+import axios from "axios";
+import dotenv from "dotenv"
+dotenv.config();
 const worker = new Worker("judge0", async job => {
+    const {token, submissionId} = job.data
+    try {
+        const options = {
+            method: 'GET',
+            url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+            params: {
+                base64_encoded: 'true',
+                fields: ''
+            },
+            headers: {
+                'x-rapidapi-key': process.env.RAPIDAPI_KEY!,
+                'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+            }
+        };
+      //  console.log("getting submission")
+        const response = await axios.request(options);
+    //    console.log(response.data.status)
+        const status = response.data.status.description
+        const statusId = response.data.status.id
+   //     console.log("updating nextjs")
+        await axios.post(process.env.NEXT_SERVER_URL!, {
+            token, submissionId, status, statusId
+        })
+   //     console.log("nextjs updated")
+        return;
+    } catch (e) {
+        console.log(e);
+    }
 
-        console.log(job.data);
-        return job.data;
 }, {
     connection : {
         host : "localhost",
@@ -12,7 +40,7 @@ const worker = new Worker("judge0", async job => {
 })
 
 worker.on('completed', job => {
-    console.log("job completed");
+    console.log("job completed", job.data);
 });
 
 worker.on('failed', (job, err) => {
